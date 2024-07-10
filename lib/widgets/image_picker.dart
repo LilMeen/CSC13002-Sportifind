a5cdbe3b9a77bd335d7baa9ff138d49b5a03e4e1
wildeuth <114043163+wildeuth@users.noreply.github.com>
2024-07-10 23:44:33 +0700

    [feature] User information: image picker in profile screen

--- lib/widgets/image_picker.dart
+++ lib/widgets/image_picker.dart
@@ -1,52 +1,18 @@
 import 'package:image_picker/image_picker.dart';
-import 'package:flutter/material.dart';
 import 'dart:io';
 
-class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key});
-  @override
-  State<UserImagePicker> createState(){
-    return _UserImagePickerState();
-  }
-}
-
-class _UserImagePickerState extends State<UserImagePicker> {
-  File? _pickedImageFile;
-
-  void _pickImage() async {
+class UserImagePicker {
+  Future<File?> pickImage() async {
     final pickedImage = await ImagePicker().pickImage(
-      source: ImageSource.camera,
+      source: ImageSource.gallery, // Changed to gallery for selecting images
       imageQuality: 50,
       maxWidth: 150,
     );
 
-    if(pickedImage == null){
-      return;
+    if (pickedImage == null) {
+      return null;
     }
 
-    setState(() {
-      _pickedImageFile = File(pickedImage.path);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return Column(children: [
-      CircleAvatar(
-        radius: 40,
-        backgroundColor: Colors.grey,
-        foregroundImage: _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
-      ),
-      TextButton.icon(
-        onPressed: (){},
-        icon: const Icon(Icons.create_rounded),
-        label: const Text(
-          'Add Image',
-          style: TextStyle(
-            color:Colors.black,
-          ),
-        ),
-      ),
-    ],);
+    return File(pickedImage.path);
   }
 }
