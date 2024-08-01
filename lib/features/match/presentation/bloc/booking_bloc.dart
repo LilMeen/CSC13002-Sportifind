267c0806ffd8f785e9d40404f5cd874c5bb14d52
CISTILY <lebathanh.nv@gmail.com>
2024-08-01 22:17:38 +0700

    [refactor] Match Management: add attributes for team and match
    Match:
    + Add StadiumOwner attributes
    
    Team:
    + Add incoming attributes

--- lib/features/match/presentation/bloc/booking_bloc.dart
+++ lib/features/match/presentation/bloc/booking_bloc.dart
@@ -1,27 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sportifind/models/booking_service.dart';
+import 'package:sportifind/models/match_card.dart';
+import 'package:sportifind/screens/player/match/screens/match_main_screen.dart';
+import 'package:sportifind/screens/player/match/util/booking_util.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:sportifind/core/usecases/usecase_provider.dart';
-import 'package:sportifind/core/util/booking_util.dart';
-import 'package:sportifind/features/match/domain/entities/booking_entity.dart';
-import 'package:sportifind/features/match/domain/entities/match_entity.dart';
-import 'package:sportifind/features/match/domain/usecases/create_match.dart';
-import 'package:sportifind/features/stadium/domain/entities/stadium_entity.dart';
-import 'package:sportifind/features/team/domain/entities/team_entity.dart';
-import 'package:sportifind/home/player_home_screen.dart';
 
-
-class BookingBloc extends ChangeNotifier {
-  BookingEntity bookingService;
-  BookingBloc({
+class BookingController extends ChangeNotifier {
+  BookingService bookingService;
+  BookingController({
     required this.bookingService,
     this.pauseSlots,
-    required this.selectedTeam,
     required this.selectedStadium,
+    required this.selectedStadiumOwner,
+    required this.selectedTeam,
+    required this.addMatchCard,
     required this.bookedTime,
     required this.selectedDate,
-    required this.selectedFieldNumberId,
+    required this.selectedField,
   }) {
     serviceOpening = bookingService.bookingStart;
     serviceClosing = bookingService.bookingEnd;
@@ -35,10 +31,12 @@ class BookingBloc extends ChangeNotifier {
 
   late DateTime base;
 
-  final TeamEntity selectedTeam;
-  final StadiumEntity selectedStadium;
+  final String selectedTeam;
+  final String selectedStadium;
   final DateTime selectedDate;
-  final int selectedFieldNumberId;
+  final String selectedStadiumOwner;
+  final String selectedField;
+  final void Function(MatchCard matchcard) addMatchCard;
   final user = FirebaseAuth.instance.currentUser!;
 
   DateTime? serviceOpening;
@@ -59,7 +57,6 @@ class BookingBloc extends ChangeNotifier {
 
   bool _successfullUploaded = false;
   bool get isSuccessfullUploaded => _successfullUploaded;
-
 
   void initBack() {
     _isUploading = false;
@@ -98,11 +95,11 @@ class BookingBloc extends ChangeNotifier {
     if (serviceOpening != null && serviceClosing != null) {
       openingHours = DateTimeRange(start: serviceOpening!, end: serviceClosing!)
           .duration
-          .inMinutes;
+          .inHours;
     }
 
     ///round down if not the whole service would fit in the last hours
-    return ((openingHours + 30) / bookingService.serviceDuration).round();
+    return ((openingHours * 60) / bookingService.serviceDuration).floor();
   }
 
   bool isSlotBooked(int index) {
@@ -143,23 +140,14 @@ class BookingBloc extends ChangeNotifier {
           isSlotBooked(index) == false) {
         result = true;
         return result;
-      } else if (index != 0 &&
-          isSlotBooked(index - 1) == false &&
-          isSlotBooked(index + 1) == true &&
-          isSlotBooked(index) == false) {
-        result = true;
-        return result;
       }
     } else if (selectedPlayTime == 90) {
       if (index == 0 &&
-          (isSlotBooked(index + 1) == true ||
-              isSlotBooked(index + 2) == true) &&
+          isSlotBooked(index + 2) == true &&
           isSlotBooked(index) == false) {
         result = true;
         return result;
-      } else if (index != 0 &&
-          isSlotBooked(index) == false &&
-          index < allBookingSlots.length - 2) {
+      } else if (index != 0 && isSlotBooked(index) == false && index < 28) {
         for (var i = 1; i <= 2; i++) {
           if (isSlotBooked(index + i) == true) {
             result = true;
@@ -169,15 +157,11 @@ class BookingBloc extends ChangeNotifier {
       }
     } else if (selectedPlayTime == 120) {
       if (index == 0 &&
-          (isSlotBooked(index + 1) == true ||
-              isSlotBooked(index + 2) == true ||
-              isSlotBooked(index + 3) == true) &&
+          isSlotBooked(index + 3) == true &&
           isSlotBooked(index) == false) {
         result = true;
         return result;
-      } else if (index != 0 &&
-          isSlotBooked(index) == false &&
-          index < allBookingSlots.length - 3) {
+      } else if (index != 0 && isSlotBooked(index) == false && index < 27) {
         for (var i = 1; i <= 3; i++) {
           if (isSlotBooked(index + i) == true) {
             result = true;
@@ -200,6 +184,11 @@ class BookingBloc extends ChangeNotifier {
     notifyListeners();
   }
 
+  // void toggleUploading() {
+  //   _isUploading = !_isUploading;
+  //   notifyListeners();
+  // }
+
   void generateBookedSlots(List<DateTimeRange> data) {
     bookedSlots.clear();
     _generateBookingSlots();
@@ -217,7 +206,7 @@ class BookingBloc extends ChangeNotifier {
     return '$hours${hours > 0 ? ':' : ''}$minutesStr';
   }
 
-  BookingEntity generateNewBookingForUploading(int selectedPlayTime) {
+  BookingService generateNewBookingForUploading(int selectedPlayTime) {
     final bookingDate = allBookingSlots.elementAt(selectedSlot);
     bookingService
       ..bookingStart = (bookingDate)
@@ -225,22 +214,43 @@ class BookingBloc extends ChangeNotifier {
     return bookingService;
   }
 
-  Future<void> createMatch(BookingEntity bookingDate, int selectedPlayTime) async {
-    await UseCaseProvider.getUseCase<CreateMatch>().call(
-      CreateMatchParams(
-        stadium: selectedStadium,
-        fieldNumberId: selectedFieldNumberId,
-        date: DateFormat.yMd().format(selectedDate),
-        start: DateFormat.Hm().format(bookingDate.bookingStart),
-        end: DateFormat.Hm().format(bookingDate.bookingEnd),
-        playTime: convertMinutesToDurationString(selectedPlayTime),
-        team1: selectedTeam,
-      ),
+  void addData(BookingService bookingDate, int selectedPlayTime) {
+    MatchCard newMatchCard = MatchCard(
+      stadium: selectedStadium,
+      stadiumOwner: selectedStadiumOwner,
+      start: formattedTime.format(bookingDate.bookingStart),
+      end: formattedTime.format(bookingDate.bookingEnd),
+      date: formatter.format(selectedDate),
+      playTime: convertMinutesToDurationString(selectedPlayTime),
+      avatarTeam1: 'lib/assets/logo/real_madrid.png',
+      team1: selectedTeam,
+      avatarTeam2: 'lib/assets/logo/logo.png',
+      team2: "?",
+      userId: user.uid,
+      field: selectedField,
     );
+    addMatchCard(newMatchCard);
+
+    FirebaseFirestore.instance.collection('matches').doc(newMatchCard.id).set({
+      'stadium': newMatchCard.stadium,
+      'stadiumOwner': newMatchCard.stadiumOwner,
+      'start': newMatchCard.start,
+      'end': newMatchCard.end,
+      'playTime': newMatchCard.playTime,
+      'date': newMatchCard.date,
+      'team1': newMatchCard.team1,
+      'team2': newMatchCard.team2,
+      'team1_avatar': newMatchCard.avatarTeam1,
+      'team2_avatar': newMatchCard.avatarTeam2,
+      'userId': user.uid,
+      'field': selectedField,
+    });
+
+    updateTeamsWhereCaptainIsUser(newMatchCard);
   }
 
   // Assume `user` is already defined and has a `uid` field
-  Future<void> updateTeamsWhereCaptainIsUser(MatchEntity newMatchCard) async {
+  Future<void> updateTeamsWhereCaptainIsUser(MatchCard newMatchCard) async {
     String userUid = user.uid;
     // Reference to the Firestore collection
     CollectionReference teamsCollection =
@@ -251,15 +261,23 @@ class BookingBloc extends ChangeNotifier {
 
     // Get the query snapshot
     QuerySnapshot querySnapshot = await query.get();
-    List<QueryDocumentSnapshot> doc = querySnapshot.docs;
-    DocumentReference docRef = doc[0].reference;
-    await docRef.update({'incomingMatch.${newMatchCard.id}': false});
+
+    // Loop through the documents and update each one
+    for (QueryDocumentSnapshot doc in querySnapshot.docs) {
+      // Get the document reference
+      DocumentReference docRef = doc.reference;
+
+      // Update the document (example: updating a field 'status' to 'active')
+      await docRef.update({
+        'incoming' : {newMatchCard.id : false}, // replace with the actual field(s) you want to update
+      });
+    }
   }
 
   void returnToMainScreen(BuildContext context) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => const PlayerHomeScreen()),
+      MaterialPageRoute(builder: (context) => const MatchMainScreen()),
     );
   }
 }
