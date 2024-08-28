0413e31520f9d42eba0b209af92bc339b8328b25
wildeuth <114043163+wildeuth@users.noreply.github.com>
2024-08-28 15:41:53 +0700

    [style] update new role screen UI, fix number wheel, fix profile display full hexagon

--- lib/features/auth/presentations/screens/role_screen.dart
+++ lib/features/auth/presentations/screens/role_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sportifind/core/theme/sportifind_theme.dart';
 import 'package:sportifind/features/auth/presentations/bloc/auth_bloc.dart';
 
@@ -14,75 +15,139 @@ class RoleScreen extends StatefulWidget {
 }
 
 class _RoleScreenState extends State<RoleScreen> {
-  @override  
-  Widget build (context){
+  String? selectedRole;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container( 
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          color: Color.fromRGBO(33, 33, 33, 1),
-        ),
-        child: Column (
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(
-              image: AssetImage('lib/assets/logo/logo.png'),
-              height: 100,
-            ),
-            const SizedBox(height: 200),
-            SizedBox(
-              height: 100,
-              width: 300,
-              child: ElevatedButton(
-                onPressed: () async{
-                  await AuthBloc(context).setRole("player");
-                },
-                style:  ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(217, 217, 217, 1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 20,
-                  ),
-                ),
-                child: const Text(
-                  'I am a Player',
-                  textAlign: TextAlign.center,
-                  style: SportifindTheme.headline,
-                )
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 40),
+              Text(
+                "What's your role?",
+                style:
+                    SportifindTheme.normalTextBlackW700.copyWith(fontSize: 32),
+                textAlign: TextAlign.center,
               ),
-            ),          
-            const SizedBox(height: 30),
-            SizedBox(
-              height: 100,
-              width: 300,
-              child: ElevatedButton(
+              const SizedBox(height: 20),
+              Text(
+                "Please select your role to continue",
+                style: SportifindTheme.normalTextBlack,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildRoleCard(
+                    title: "Stadium owner",
+                    icon: Icons.stadium,
+                    isSelected: selectedRole == "stadium_owner",
+                    onTap: () => _toggleRole("stadium_owner"),
+                  ),
+                  const SizedBox(height: 20),
+                  _buildRoleCard(
+                    title: "Player",
+                    icon: Icons.sports_soccer,
+                    isSelected: selectedRole == "player",
+                    onTap: () => _toggleRole("player"),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              ElevatedButton(
                 onPressed: () async {
-                  await AuthBloc(context).setRole('stadium_owner');
+                  await AuthBloc(context).setRole(selectedRole!);
                 },
-                style:  ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(217, 217, 217, 1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 20,
-                  ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: SportifindTheme.bluePurple,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                child: Text(
+                  "Continue",
+                  style: SportifindTheme.normalTextWhite.copyWith(fontSize: 20),
                 ),
-                child: const Text(
-                  'I am a Stadium owner',
-                  textAlign: TextAlign.center,
-                  style: SportifindTheme.headline,
-                )
               ),
-            ),          
-          ]
-        )
-      )
+            ],
+          ),
+        ),
+      ),
     );
   }
-}
 
+  Widget _buildRoleCard({
+    required String title,
+    required IconData icon,
+    required bool isSelected,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: 240, 
+        height: 165,
+        child: Card(
+          color: Colors.white,
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+            side: BorderSide(
+              color:
+                  isSelected ? SportifindTheme.bluePurple : Colors.grey,
+              width: 1,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: GoogleFonts.rowdies(
+                    fontSize: 24,
+                    color:
+                        isSelected ? SportifindTheme.bluePurple : Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Divider(color: isSelected ? SportifindTheme.bluePurple : Colors.grey),
+                const SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: title == 'Stadium owner'
+                    ? Icon(
+                        icon,
+                        size: 50,
+                        color: isSelected ? SportifindTheme.bluePurple : Colors.grey,
+                      )
+                    : Image.asset(
+                        isSelected ? 'lib/assets/player_picked.png' : 'lib/assets/player_unpicked.png',
+                        width: 70,
+                        height: 55,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _toggleRole(String role) {
+    setState(() {
+      if (selectedRole == role) {
+        selectedRole = null;
+      } else {
+        selectedRole = role;
+      }
+    });
+  }
+}
