5a5823df01a713e43cae4cb431c7c7e89abbfe83
LilMeen <nhkminh2004@gmail.com>
2024-09-17 19:29:18 +0700

    delete unuse file and folders

--- lib/screens/message/util/database_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class MessageService {
-  final CollectionReference userCollection =
-      FirebaseFirestore.instance.collection('users');
-  final CollectionReference teamsCollection =
-      FirebaseFirestore.instance.collection('teams');
-
-}
